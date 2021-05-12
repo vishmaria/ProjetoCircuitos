2d16e41831ec07228123c740e16efd162ddd3115
vishmaria <maria.fernandabt99@gmail.com>
2021-05-12 14:50:14 -0300

    varios port maps

--- datapath.vhd
+++ datapath.vhd
@@ -13,10 +13,10 @@ end datapath;
 
 architecture arqdata of datapath is
 
-    signal seq_fpga: std_logic_vector(17 downto 0);
-    signal seq1_out, seq2_out, seq3_out, seq4_out: std_logic_vector(17 downto 0);   
-    signal round: std_logic_vector(3 downto 0);
+    signal seq_fpga, seq1_out, seq2_out, seq3_out, seq4_out: std_logic_vector(17 downto 0);   
+    signal round, time: std_logic_vector(3 downto 0);
     signal round_bcd: std_logic_vector(7 downto 0);
+    signal sum_out, contagem: std_logic_vector(5 downto 0);
 
     component mux2_1 is port (
     	F1: in std_logic_vector(17 downto 0);
@@ -104,23 +104,26 @@ architecture arqdata of datapath is
 
     begin
 
-    CR: contador_round port map ("0000", e1, clk50, e4, round, end_round); 
+    Clevel: contador_crescente port map (sw_entra(9 downto 6), (r1 or e4), clk1, e2, "0000", end_FPGA );
+    Ctime: contador_crescente port map ("1010",(r1 or e4),clk1, e3, time, end_time);    
+    Cround: contador_round port map ("0000", e1, clk50, e4, round, end_round); 
     S1: SEQ1 port map (round, seq1_out);
     S2: SEQ2 port map (round, seq2_out);   
     S3: SEQ3 port map (round, seq3_out);
     S4: SEQ4 port map (round, seq4_out);
 
+    MUXrom: mux4_1 port map (seq1_out, seq2_out, seq3_out, seq4_out, sw_entra(5 downto 4), seq_fpga);  
+    Sum: sum port map ((seq_fpga xor sw_entra(17 downto 0)),sum_out);
+    Cbonus: contador_bonus port map (sum_out, sw_entra(13 downto 10), e1, clk50, (e3 and not(key_entra)), contagem, end_bonus);
+    REG:
+
     DECbcd: decodificador port map (round, round_bcd); 
 
-    MUX4: mux4_1 port map (seq1_out, seq2_out, seq3_out, seq4_out, sw_entra(5 downto 4), seq_fpga); 
-    MUX2: mux2_1 port map ("000000000000000000",seq_fpga, e2, led_out );
    
-    DEC:
-    REG:
-    Sum:
-    CC:
-    CR: 
-    CB:
+    MUXled: mux2_1 port map ("000000000000000000",seq_fpga, e2, led_out );
+   
+
+  
 end arqdata;
 
 
