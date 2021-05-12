38c98fa6d99ac05397854f353ff75250c587388b
vishmaria <maria.fernandabt99@gmail.com>
2021-05-12 15:28:02 -0300

    numeros legiveis em sist decimal

--- dec_bcd.vhd
+++ dec_bcd.vhd
@@ -1,11 +1,11 @@
 library IEEE;
 use IEEE.Std_Logic_1164.all;
-entity decodificador is
+entity dec_bcd is
 port (G: in std_logic_vector(3 downto 0);
 		out_hex: out std_logic_vector(7 downto 0)
 		);
-end decodificador;
-architecture decod of decodificador is
+end dec_bcd;
+architecture decod of dec_bcd is
 begin
 out_hex <= "00000000" when G = "0000" else -- 0
 	   "00000001" when G = "0001" else -- 1
@@ -17,10 +17,10 @@ out_hex <= "00000000" when G = "0000" else -- 0
 	   "00000111" when G = "0111" else -- 7
 	   "00001000" when G = "1000" else -- 8
 	   "00001001" when G = "1001" else -- 9
-	  -- "00001010" when G = "1010" else -- A
-	   --"00001011" when G = "1011" else -- B
-	   --"00001100" when G = "1100" else -- C
-	   --"00001101" when G = "1101" else -- D
-	   --"00001110" when G = "1110" else -- E
-	   --"00001111"; -- F	
+	   "00010000" when G = "1010" else -- A
+	   "00010001" when G = "1011" else -- B
+	   "00010010" when G = "1100" else -- C
+	   "00010011" when G = "1101" else -- D
+	   "00010100" when G = "1110" else -- E
+	   "00010101"; -- F	
 end decod;
