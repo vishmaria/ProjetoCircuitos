fea1f74e3be0d4370276d5593e089e23e0579463
vishmaria <maria.fernandabt99@gmail.com>
2021-05-13 18:53:26 -0300

    alterações não deram certo :(

--- contador_crescente.vhd
+++ contador_crescente.vhd
@@ -2,7 +2,6 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 use ieee.numeric_std.all;
-
 Entity contador_crescente is port ( 
     data: in std_logic_vector (3 downto 0);
     reset: in std_logic;
@@ -11,22 +10,22 @@ Entity contador_crescente is port (
     contagem: buffer std_logic_vector(3 downto 0);
     end_count: out std_logic);
 end contador_crescente;
-
 Architecture circuito of contador_crescente is
-    signal mem_contagem: std_logic_vector(3 downto 0);
+signal mem_contagem: std_logic_vector(3 downto 0);
+    begin
+    P1: process(clock, reset, enable, data)
         begin
-        P1: process(clock, reset, enable, data)
-            begin
-             if reset= '1' then
-                 contagem <= "0000";
-             elsif ((clock'event and clock= '1') and enable ='1') then
-                 contagem <= contagem+1;
-            end if;
-        end process;
-    if contagem = data then
-                 end_count <='1';
-                 contagem <= "0000";
-                 else
-                 end_count <= '0';
-    end if;
+         if reset= '1' then
+             contagem <= "0000";
+         elsif ((clock'event and clock= '1') and enable ='1') then
+             contagem <= contagem+1;
+        end if;
+         if contagem = data then
+             end_count <='1';
+             contagem <= "0000";
+        else
+             end_count <= '0';
+
+         end if;
+    end process;
 end circuito;
